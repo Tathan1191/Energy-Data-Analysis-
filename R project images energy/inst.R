a822936802dc53aaacf4653af9280d32cee8847c
Tathan1191 <alvarezjonnathan@gmail.com>
2023-12-30 15:36:32 -0500

    Primer commit

---
+++ R project images energy/inst.R
@@ -0,0 +1,46 @@
+#¿Cuál es la tendencia del acceso a la electricidad a nivel mundial de 2000 a 2020? 
+Cómo hacerlo: Utilizar un gráfico de líneas con ggplot2.
+Gráfico recomendado: Gráfico de líneas.
+
+#¿Existen diferencias notables en el acceso a la electricidad entre países?
+ Cómo hacerlo: Un gráfico de barras ordenado por el porcentaje de acceso a la electricidad.
+Gráfico recomendado: Gráfico de barras.
+
+#¿Hay una correlación entre el acceso a la electricidad y el PIB per cápita?
+  Cómo hacerlo: Gráfico de dispersión con una línea de regresión.
+Gráfico recomendado: Gráfico de dispersión con línea de regresión.
+
+#¿Cómo ha evolucionado la capacidad de generación de electricidad renovable per cápita a lo largo de los años?
+  Cómo hacerlo: Gráfico de líneas con ggplot2.
+Gráfico recomendado: Gráfico de líneas.
+
+#¿Existe una relación entre los flujos financieros hacia los países en desarrollo y la generación de electricidad renovable?
+  Cómo hacerlo: Gráfico de dispersión con un ajuste de línea.
+Gráfico recomendado: Gráfico de dispersión con línea de tendencia.
+
+#¿Cuál es la proporción de energía renovable en el consumo total de energía?
+  Cómo hacerlo: Gráfico de barras apiladas para mostrar la proporción.
+Gráfico recomendado: Gráfico de barras apiladas.
+
+#¿Cuánta electricidad se genera a partir de combustibles fósiles, nuclear y renovables?
+  Cómo hacerlo: Gráfico de barras agrupadas o gráfico de sectores.
+Gráfico recomendado: Gráfico de barras agrupadas o gráfico de sectores.
+
+#¿Cómo ha cambiado la generación de electricidad a partir de fuentes renovables específicas?
+  Cómo hacerlo: Gráfico de líneas múltiples para cada fuente renovable.
+Gráfico recomendado: Gráfico de líneas múltiples.
+
+#¿Cuál es la relación entre el acceso a energía limpia para cocinar y el acceso a la electricidad?
+  Cómo hacerlo: Gráfico de dispersión o gráfico de barras agrupadas.
+Gráfico recomendado: Gráfico de dispersión o gráfico de barras agrupadas.
+
+#¿Cómo ha evolucionado la participación de los países en el uso de energías renovables desde 2000?
+  Cómo hacerlo: Gráfico de líneas o gráfico de área.
+Gráfico recomendado: Gráfico de líneas o gráfico de área.
+
+#Observaciones generales:
+  
+  #Colores y estilos: Utiliza colores atractivos y estilos limpios para hacer que los gráficos sean visualmente agradables.
+Interactividad: Considera la posibilidad de hacer gráficos interactivos usando paquetes como plotly para una exploración más detallada.
+Storytelling: Acompaña los gráficos con breves narrativas que destaquen las conclusiones clave y las implicaciones de los datos.
+
